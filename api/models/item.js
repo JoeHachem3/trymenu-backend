a3bdd99093c85df7a62b7b9ab5ff81fb2a35d3bf
Joe Hachem <joehachem2000@gmail.com>
2021-04-03 22:31:42 +0300

    updated item schema + refer to its restaurant

--- api/models/item.js
+++ api/models/item.js
@@ -3,8 +3,19 @@ const mongoose = require('mongoose');
 const itemSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true, lowercase: true, trim: true },
-  //   category: { type: String, required: true, lowercase: true, trim: true },
-  //   ingredients: [{ type: mongoose.Schema.Types.ObjectId, ingredient: 'Ingredient' }],
+  price: { type: Number, required: true, trim: true },
+  restaurant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Restaurant',
+    required: true,
+  },
+  category: {
+    type: String,
+    default: undefined,
+    lowercase: true,
+    trim: true /*required: true*/,
+  },
+  ingredients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Ingredient' }],
 });
 
 module.exports = mongoose.model('Item', itemSchema);
