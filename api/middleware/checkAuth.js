c0a77facd61d3c9ee944fa42a6fd046a617feab6
Joe Hachem <joehachem2000@gmail.com>
2021-04-22 21:00:44 +0300

    .env

--- api/middleware/checkAuth.js
+++ api/middleware/checkAuth.js
@@ -9,6 +9,8 @@ module.exports = (req, res, next) => {
   } catch (error) {
     return res.status(401).json({
       message: 'Authentication failed',
+      whatever: 'hi',
+      test: process.env.MONGO_ATLAS_PW,
     });
   }
 };
